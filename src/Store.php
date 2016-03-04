24c33a90927265fd0094e9cbf013540cfbe1bc85
nic <netzeln@gmail.com>
2016-03-04 09:36:40 -0800

    re-pass get and set tests

--- src/Store.php
+++ src/Store.php
@@ -1,5 +1,27 @@
 <?php
-    class Store
-    {
+    class Store {
+        private $name;
+        private $id;
+
+        function __construct ($name, $id = NULL)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+        function getId()
+        {
+            return $this->id;
+        }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
         
-?>
+
+
+} ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,21 +1,65 @@
 <?php
 
-
 /**
 * @backupGlobals disabled
 * @backupStaticAttributes disabled
 */
 
-require_once "src/";
+require_once "src/Store.php";
 
 
-$server = 'mysql:host=localhost;dbname=';
+$server = 'mysql:host=localhost;dbname=shoes_test';
 $username = 'root';
 $password = 'root';
 $DB = new PDO($server, $username, $password);
 
 
 
+class  StoreTest  extends PHPUnit_Framework_TestCase{
 
+    function testGetName()
+    {
+        //arrange
+        $name = "Basie's Boots";
+        $id = NULL;
+        $test_store = new Store($name, $id);
+
+        //act
+        $result = $test_store->getName();
+
+        //assert
+        $this->assertEquals($name, $result);
+
+    }
+    function testGetId()
+    {
+        //arrange
+        $name = "Basie's Boots";
+        $id = 1;
+        $test_store = new Store($name, $id);
+
+        //act
+        $result = $test_store->getId();
+
+        //assert
+        $this->assertEquals($id, $result);
+
+    }
+    function testSetName()
+    {
+        //arrange
+        $name = "Basie's Boots";
+        $id = NULL;
+        $test_store = new Store($name, $id);
+
+        $new_name ="Boots by Basie";
+
+        //act
+        $result = $test_store->setName($new_name);
+
+        //assert
+        $this->assertEquals($new_name, $result);
+
+    }
 }
  ?>
